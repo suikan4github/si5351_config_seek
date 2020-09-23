edfa75f2f043904af0a504cf2fed5b26fedeb21f
Suikan <26223147+suikan4github@users.noreply.github.com>
2020-09-23 23:24:04 +0900

    Added si5351configseek.cpp

--- inc/si5351configseek.hpp
+++ inc/si5351configseek.hpp
@@ -6,24 +6,23 @@
 enum Si5351Status {
     s5351Ok,
     s5351SeekFailure
-}
+};
 
 /**
  * @brief Seek the appropriate configuration of the Si5351.
- * @param xtal_freq Pass the input Xtal frequency in Hz. Must be from 10MHz to 40Mhz.
- * @param output_freq Pass the ouput Xtal frequency in Hz. Must be from 2.5kHz 200MHz.
+ * @param xtal_freq Input : Xtal frequency in Hz. Must be from 10MHz to 40Mhz.
+ * @param output_freq Input : Output frequency in Hz. Must be from 2.5kHz 200MHz.
  * @param integer_mode Pass true if the output divide ratio must be integer. 
- * @param stage1_a Output : Parameter "a" of the PLL feedback divider.
- * @param stage1_b Output : Parameter "b" of the PLL feedback divider.
- * @param stage1_b Output : Parameter "c" of the PLL feedback divider.
- * @param stage2_a Output : Parameter "a" of the stage 2 divider.
- * @param stage2_b Output : Parameter "b" of the stage 2 divider.
- * @param stage2_c Output : Parameter "c" of the stage 2 divider.
+ * @param stage1_a Output : Parameter "a" of the PLL feedback divider. [15, 90]
+ * @param stage1_b Output : Parameter "b" of the PLL feedback divider. [0, 1048575]
+ * @param stage1_b Output : Parameter "c" of the PLL feedback divider. [0, 1048575]
+ * @param stage2_a Output : Parameter "a" of the stage 2 divider. [8, 2048]
+ * @param stage2_b Output : Parameter "b" of the stage 2 divider. [0, 1048575]
+ * @param stage2_c Output : Parameter "c" of the stage 2 divider. [0, 1048575]
  * @param div_by_4 Output : 0b11 : div by 4 mode. 0b00 : div by other mode.
- * @param 
  * @param r Output : Output divider
  * @details 
- * Calcurate an appropriate set of the PLL parameters for Si5351. 
+ * Calcurate an appropriate set of the PLL parameters for Si5351. See AN619 for detail of Si5351.
  * 
  * This function calcurate the formula only. So, the atuall register 
  * configuration have to be compiled from the given parameters. 
@@ -48,22 +47,22 @@ enum Si5351Status {
  *  Fout = xtal_freq * ( stage1_a + stage1_b/stage1_c) /( (stage2_a + stage2_b/stage2_c) * r )
  * 
  * Also, there are several restriction by silicon.
- * @li If the output is below 500kHz, use r divider 2, 4, 8, ... 128. 
+ * @li If the output is below 500kHz, use r divider 1, 2, 4, 8, ... 128. 
  * @li If the output is over 150MHz, stage 2 is a=0, b=0, c=0, div_by_4=0b11
  */
-enum Si5351Status Si5351ConfigSeek(
-    int32_t xtal_freq,
-    int32_t output_freq,
-    bool integer_mode,
+Si5351Status Si5351ConfigSeek(
+    const int32_t xtal_freq,
+    const int32_t output_freq,
+    const bool integer_mode,
     int32_t & stage1_a,
     int32_t & stage1_b,
     int32_t & stage1_c,
     int32_t & stage2_a,
     int32_t & stage2_b,
     int32_t & stage2_c,
-    in532_t & div_by_4,
+    int32_t & div_by_4,
     int32_t & r
-)
+);
 
 
 #endif // SI5351_CONFIG_SEEK_HPP
---
+++ src/si5351configseek.cpp
@@ -0,0 +1,55 @@
+#include <si5351configseek.hpp>
+
+#define SI5351_ASSERT(x)
+#define SI5351_SYSLOG(x, y)
+
+Si5351Status Si5351ConfigSeek(
+    const int32_t xtal_freq,
+    const int32_t output_freq,
+    const bool integer_mode,
+    int32_t &stage1_a,
+    int32_t &stage1_b,
+    int32_t &stage1_c,
+    int32_t &stage2_a,
+    int32_t &stage2_b,
+    int32_t &stage2_c,
+    int32_t &div_by_4,
+    int32_t &r)
+{
+    // Because the div_by_4 mode is tricky, keep second stage divider value here.
+    int32_t second_stage_divider;
+
+    SI5351_ASSERT(output_freq > 2);         // must be higher than 2Hz.
+    SI5351_ASSERT(200000000 > output_freq); // must be lower than or equal to 200MHz
+
+    SI5351_SYSLOG("Xtal Frequency is %d Hz", xtal_freq);
+    SI5351_SYSLOG("Output Frequency is %d Hz", output_freq);
+
+    if (output_freq > 150000000) // If output freq is > 150MHz
+    {
+        SI5351_SYSLOG("Frequency is higher than 150MHz");
+
+        SI5351_SYSLOG("Set the stage 2 to divid by 4 mode")
+        // enforce divide by 4 mode.
+        // Note that followings are required by Si5351 data sheet.
+        // In case of divide by 4 mode, the second stage have to be integer mode.
+        div_by_4 = 3; // requed by Si5351 data sheet
+        stage2_a = 0;
+        stage2_b = 0;
+        stage2_c = 1;
+
+        // For output freq > 150MHz, r is set to 1;
+        SI5351_SYSLOG( "Set r as 1");
+        r = 1;
+
+        second_stage_divider = 4;
+    }
+    else
+    {
+        // At first, roughly calculate the dividing value.
+        second_stage_divider = 1 + 600000000 / output_freq;
+        r = 1;
+    }
+
+    return s5351Ok;
+}
