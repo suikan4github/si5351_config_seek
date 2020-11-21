9d70ffc27004de3ef097f25b0422565bc34514be
Suikan <26223147+suikan4github@users.noreply.github.com>
2020-11-21 15:54:53 +0900

    Test passed.
    from 100kHz to 120MHz, 1kHz step

--- test/main.cpp
+++ test/main.cpp
@@ -13,32 +13,34 @@ int main()
     int32_t div_by_4;
     int32_t r;
 
-    Si5351ConfigSeek(
-        XTAL_FREQ,
-        OUTPUT_FREQ,
-        true, // integer mode.
-        stage1_a,
-        stage1_b,
-        stage1_c,
-        stage2_a,
-        stage2_b,
-        stage2_c,
-        div_by_4, // 0b11:div by 4, 0b:00 div by other.
-        r         // output divider.
-    );
-
-    double output;
-    if (div_by_4 == 3)
-        output = XTAL_FREQ * (stage1_a + double(stage1_b) / stage1_c) / (stage2_a) / r;
-    else if (div_by_4 == 0)
-        output = XTAL_FREQ * (stage1_a + double(stage1_b) / stage1_c) / (stage2_a) / r;
-    else
+    for (int i = 100000; i < 120000000; i += 1000)
     {
-        printf("error \n");
-        /* code */
-    }
 
-    printf("output freq : %e \n", output);
+        Si5351ConfigSeek(
+            XTAL_FREQ,
+            i,
+            true, // integer mode.
+            stage1_a,
+            stage1_b,
+            stage1_c,
+            stage2_a,
+            stage2_b,
+            stage2_c,
+            div_by_4, // 0b11:div by 4, 0b:00 div by other.
+            r         // output divider.
+        );
+
+        double output;
+        if (div_by_4 == 3)
+            output = XTAL_FREQ * (stage1_a + double(stage1_b) / stage1_c) / 4;
+        else if (div_by_4 == 0)
+            output = XTAL_FREQ * (stage1_a + double(stage1_b) / stage1_c) / (stage2_a) / r;
+        else
+            printf("logic error \n");
+
+        if ((double)i != output)
+            printf("Error at %dkHz, output is %ekHz \n", i / 1000, output / 1000);
+    }
 
     return 0;
 }
